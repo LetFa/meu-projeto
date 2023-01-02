17ffc04c388d3004940d361f48e2c9ee1b9b0504
leticia <lefarias111@gmail.com>
2023-01-02 13:09:03 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+  const name = 'luciana'
+  const nameM = name.toUpperCase()
+
+
+  function soma(a,b){
+    return a * b
+  }
+
+  const URL = "https://images.pexels.com/photos/6829488/pexels-photo-6829488.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
   return (
     <div className="App">
-       <h1>Olá React!</h1>
-       <p>Meu primeiro App</p>
+       <h2>Alterando JSX</h2>
+       <p>paragrafo</p>
+       <p>Olá, {name}</p>
+       <p>Nome em letra grande, {nameM}</p>
+       <p>{soma(50,10)}</p>
+       <img src={URL}  />
     </div>
   );
 }
