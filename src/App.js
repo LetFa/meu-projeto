4a0cbf07fed88d5ab057177beeaa26d6cb0f08db
leticia <lefarias111@gmail.com>
2023-01-02 17:18:30 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import './App.css';
+import './App.css'
+import HelloWorld  from './components/HelloWorld'
+
 
 function App() {
-  const name = 'luciana'
+
+  const name = "Letícia Farias"
   const nameM = name.toUpperCase()
-
-
-  function soma(a,b){
-    return a * b
+  
+  function soma(a, b){
+   return  a + b
   }
+  
+  const URL = "https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg?auto=compress&cs=tinysrgb&w=200"
 
-  const URL = "https://images.pexels.com/photos/6829488/pexels-photo-6829488.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
   return (
     <div className="App">
-       <h2>Alterando JSX</h2>
-       <p>paragrafo</p>
-       <p>Olá, {name}</p>
-       <p>Nome em letra grande, {nameM}</p>
-       <p>{soma(50,10)}</p>
-       <img src={URL}  />
+
+       <h2 className="text-success mt-5">Estudando React</h2>
+       <p>alterando JSX</p>
+      <h5>Criando variáveis</h5>
+      <p>Olá, {name}</p>
+      <h5>Criando nome com letra maiúscula</h5>
+      <p>Olá, {nameM}</p>
+      <h5>Criando função</h5>
+      <p>Soma: {soma(50,50)}</p>
+      <h5>Imagem</h5>
+      <img src={URL} alt="imagem react" />
+      <HelloWorld />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+      <div>
+        <p>em 2023 estou trabalhando na empresa winner Vendas</p>
+      </div>
+    );
+}
+
+export default Frase
